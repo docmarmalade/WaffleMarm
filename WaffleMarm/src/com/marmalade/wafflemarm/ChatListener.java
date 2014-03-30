0ff6e41571f09f06ab1cc8f9eb24379be00ea3ee
docmarmalade <mark.meyer1014@gmail.com>
2014-03-30 13:26:19 -0400

    Eliminated JoinListener, deleted command bye from yml, recoded listener and main, still doesn't work yet

--- WaffleMarm/src/com/marmalade/wafflemarm/ChatListener.java
+++ WaffleMarm/src/com/marmalade/wafflemarm/ChatListener.java
@@ -1,51 +1,36 @@
 package com.marmalade.wafflemarm;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-public class ChatListener implements Listener {
-	
-	public WaffleMarm plugin;
+public final class ChatListener implements Listener {
+
+	final WaffleMarm plugin;
 	
 	public ChatListener(WaffleMarm plugin){
 		this.plugin = plugin;
 	}
-		
-	@EventHandler(priority = EventPriority.LOW)
+
+	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerChat(AsyncPlayerChatEvent event) {
-		event.setCancelled(true);
+
 		Player player = event.getPlayer();
 		
-		if(player.getName().equalsIgnoreCase("dr_muffinbutter")){
-		
-			event.setFormat(ChatColor.GOLD + "[" + ChatColor.GREEN + "Developer" + ChatColor.GOLD + "]" 
-			         + ChatColor.RED + player.getName() + " " + ChatColor.YELLOW+ event.getMessage());
+		if (event.getMessage().startsWith("/bye")){
+			if(event.isCancelled()) return;
+			player.sendMessage("Goodbye");
 		}
-		else if (player.isOp()){
-			event.setFormat(ChatColor.GOLD + "[" + ChatColor.BLUE + "Op" + ChatColor.GOLD + "]"
-					 + ChatColor.WHITE + player.getName() + " " + ChatColor.AQUA + event.getMessage());
-			
-		}
-		else {
-			event.setFormat(ChatColor.GRAY + "[Player]" + ChatColor.RESET + player.getName() + " " + ChatColor.AQUA + event.getMessage());
-					
-					}
-		
-		String chat = plugin.chat.get(player.getName());
-		
-	    for (Player p : Bukkit.getServer().getOnlinePlayers()){
-	    	
-			if (plugin.chat.get(p.getName()).equals(chat)){
-	        	 
-	        	 p.sendMessage(chat + event.getFormat());
-	    		
-	    		
-	    	}
-	    }
-    }
+
+	}
 }
+
+
+
+
+
+
+
+
--- WaffleMarm/src/com/marmalade/wafflemarm/Commands.java
+++ WaffleMarm/src/com/marmalade/wafflemarm/Commands.java
@@ -6,39 +6,39 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class Commands implements CommandExecutor{
-	
+
 	public WaffleMarm plugin; 
-	
+
 	public Commands(WaffleMarm plugin){
 		this.plugin = plugin;
 	} //end Commands
-	
+
 	@Override 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-        
-		Player player = (Player) sender;
-		
-		if (label.equalsIgnoreCase("chat")) {
-        	if(args.length == 1){
-        		plugin.chat.put(player.getName(), args[0].toLowerCase());
-              } //end if
-            else {
-        	plugin.chat.put(player.getName(), "default");
-              } //end else
-		} //end if label
-		return true;
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+
+		if(cmd.getName().equalsIgnoreCase("hi")){	
+			if(sender instanceof Player){
+				String displayName = sender.getName();
+				sender.sendMessage("Hi " + displayName + "!");
+				return true;
+			}
+			else{
+				sender.sendMessage("You must be a player to use this command.");
+			}
+		}
+		return false;
 	}
 }
-        
-        
-       
-        
-	
-
-        
 
 
-	
-    	
-    		
+
+
+
+
+
+
+
+
+
+
 
