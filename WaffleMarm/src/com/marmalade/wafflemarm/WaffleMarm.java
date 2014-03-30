f7e59ef0d70d3cd7767d30e961d9727cbacb6664
docmarmalade <mark.meyer1014@gmail.com>
2014-03-30 17:29:07 -0400

    player now dies when a block is placed.

--- WaffleMarm/src/com/marmalade/wafflemarm/WaffleMarm.java
+++ WaffleMarm/src/com/marmalade/wafflemarm/WaffleMarm.java
@@ -10,6 +10,7 @@ public final class WaffleMarm extends JavaPlugin {
 	public static WaffleMarm plugin;
 	public final Logger log = Logger.getLogger("Minecraft");
 	private final PlayerListener playerListener = new PlayerListener(this);
+	private final BlockListener blockListener = new BlockListener(this);
 	public WaffleMarm() {
 		plugin = this;
 	}
@@ -19,8 +20,10 @@ public final class WaffleMarm extends JavaPlugin {
 		getLogger().info("It's Alive!!");
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(playerListener, this);
+		pm.registerEvents(blockListener, this);
 		getCommand("hi").setExecutor(new Commands(this));
 		getCommand("jump").setExecutor(new Commands(this));
+		
 	}
 }
 
