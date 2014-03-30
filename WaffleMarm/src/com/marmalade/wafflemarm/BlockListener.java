ae6322dbbb10af1c1477490d8ca9fed36d6ef338
docmarmalade <mark.meyer1014@gmail.com>
2014-03-30 17:29:30 -0400

    player now dies when a block is placed

---
+++ WaffleMarm/src/com/marmalade/wafflemarm/BlockListener.java
@@ -0,0 +1,31 @@
+package com.marmalade.wafflemarm;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+	public final class BlockListener implements Listener {
+
+		final WaffleMarm plugin;
+		
+		public BlockListener(WaffleMarm instance) {
+			plugin = instance;
+		}
+
+		@EventHandler(priority = EventPriority.NORMAL)
+		public void onBlockPlace(BlockPlaceEvent event) {
+			
+			Player player = event.getPlayer();
+			
+			event.setCancelled(true);
+				player.setHealth(0.0);
+				player.sendMessage("Hahaha you fool.");
+			}
+
+		}
+	
+
+
+
