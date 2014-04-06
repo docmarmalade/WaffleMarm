66854f03143301ee46497655319cacce02c4a045
docmarmalade <mark.meyer1014@gmail.com>
2014-04-06 18:43:59 -0400

    updated 4/6 1843 est

--- WaffleMarm/src/com/marmalade/wafflemarm/PlayerListener.java
+++ WaffleMarm/src/com/marmalade/wafflemarm/PlayerListener.java
@@ -1,5 +1,8 @@
 package com.marmalade.wafflemarm;
 
+import org.bukkit.Chunk;
+import org.bukkit.block.Biome;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,21 +14,38 @@ import com.marmalade.wafflemarm.WaffleMarm;
 public final class PlayerListener implements Listener {
 
 	final WaffleMarm plugin;
-	
+
 	public PlayerListener(WaffleMarm instance) {
 		plugin = instance;
 	}
 
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
-		
+
 		Player player = event.getPlayer();
-		
+
 		if (event.getMessage().startsWith("/bye")){
 			player.sendMessage("Goodbye");
 			event.setCancelled(true);
 		}
 
+		if(player.isOp()){
+			if (event.getMessage().startsWith("/desert")) {
+				player.sendMessage("I shall bring you the desert..");
+				int count = 0;
+				for (Chunk chunk : player.getWorld().getLoadedChunks()) {
+					for (int x = 0; x < 16; x = x + 1) {
+						for (int z = 0; z < 16; z = z + 1) {
+							Block block = chunk.getBlock(x, 0, z);
+							block.setBiome(Biome.DESERT);
+						}
+					}
+					count++;
+				} 
+				player.sendMessage("Chunks Enhanced: " + count);
+				event.setCancelled(true);
+			}
+		}
 	}
 }
 
