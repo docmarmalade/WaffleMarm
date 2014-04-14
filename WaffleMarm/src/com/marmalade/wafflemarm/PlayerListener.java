c80ac381cbfd047c9f3f2b6a19e5ea6c8bd6ca19
docmarmalade <mark.meyer1014@gmail.com>
2014-04-14 17:46:55 -0400

    updating

--- WaffleMarm/src/com/marmalade/wafflemarm/PlayerListener.java
+++ WaffleMarm/src/com/marmalade/wafflemarm/PlayerListener.java
@@ -32,38 +32,41 @@ public class PlayerListener implements Listener {
 			player.sendMessage("Goodbye");
 			event.setCancelled(true);
 		}
-		for(Chunk chunk : player.getWorld().getLoadedChunks()){
-			for(int x = 0; x < 16; x++) {                 //iterating through chunks
-				for(int z = 0; z < 16; z++) {  
-					Block block = chunk.getBlock(x,0,z);        //setting its biome via blocks
-					block.setBiome(Biome.DESERT);  
-					Chunk chunkCoords = world.getChunkAt(block);  //get chunk from block from loaded chunks
-					int chunkx = chunkCoords.getX();
-					int chunkz = chunkCoords.getZ();
-					String coordSet = (chunkx + "," + chunkz);    //store set of coords as string
-					WaffleMarm.newChunks.add(coordSet);
-					count++;
-					WaffleMarm.oldChunks.add(coordSet);
-					if (event.getMessage().startsWith("/desert start")) {
-						player.sendMessage("This biome is now a desert. Good Luck.");
-						if(!(coordSet.contains(coordSet))){
-							player.sendMessage("Chunk Coordinates Enhanced: " + WaffleMarm.oldChunks);
+
+		if(player.isOp()){
+			if (event.getMessage().startsWith("/desert start")) {
+				for(Chunk chunk : player.getWorld().getLoadedChunks()){
+					for(int x = 0; x < 16; x++) {                 //iterating through chunks
+						for(int z = 0; z < 16; z++) {  
+							Block block = chunk.getBlock(x,0,z);        //setting its biome via blocks
+							block.setBiome(Biome.DESERT);  
+							Chunk chunkCoords = world.getChunkAt(block);  //get chunk from block from loaded chunks
+							int chunkx = chunkCoords.getX();
+							int chunkz = chunkCoords.getZ();
+							String coordSet = (chunkx + "," + chunkz);    //store set of coords as string
+							WaffleMarm.newChunks.add(coordSet);
+							count++;
+							WaffleMarm.oldChunks.add(coordSet);
+							if(!(coordSet.contains(coordSet))){
+								player.sendMessage("Chunk Coordinates Enhanced: " + WaffleMarm.oldChunks);
+							}
+							else{
+								player.sendMessage("No Chunks Coordinates Enhanced.");
+							}
+							event.setCancelled(true);
 						}
-						event.setCancelled(true);
 					}
 				}
 			}
-		}
-		
-		if(player.isOp()){
 			if (event.getMessage().startsWith("/desert changed")) {
-
 				if(!(WaffleMarm.newChunks.contains(WaffleMarm.oldChunks))){    //making sure there are no duplicates
 					player.sendMessage("New Chunks Processed: " + (count/65536));   //(16*16*256)blocks in a chunk =65536 blocks, count uses for-loop that iterates through block so this translates block count into chunk count 
 				}
+				else{
+					player.sendMessage("No new chunks processed");
+				}
 				event.setCancelled(true);
-			}                                                                  
-			
+			}
 		}
 	}
 }
