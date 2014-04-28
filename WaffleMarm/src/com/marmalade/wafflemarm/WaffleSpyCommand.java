a89cc51ba4b69f61b5c0ce64d805bd7389b54c3c
docmarmalade <mark.meyer1014@gmail.com>
2014-04-27 22:45:46 -0400

    oops forgot to delete a BIG copy/paste LOL

--- WaffleMarm/src/com/marmalade/wafflemarm/WaffleSpyCommand.java
+++ WaffleMarm/src/com/marmalade/wafflemarm/WaffleSpyCommand.java
@@ -1,21 +1,15 @@
 package com.marmalade.wafflemarm;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.net.InetAddress;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 
 public class WaffleSpyCommand implements CommandExecutor {
 
@@ -23,42 +17,6 @@ public class WaffleSpyCommand implements CommandExecutor {
 
 	WaffleSpyCommand(WaffleMarm instance) {
 		plugin = instance;
-	}
-	static File df = WaffleMarm.plugin.getDataFolder();
-
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerLogin(AsyncPlayerPreLoginEvent e){
-		InetAddress ip = e.getAddress();
-		String iP = ip.getHostName();
-		String playerName = e.getName();
-		OfflinePlayer offlinePlayer = plugin.getServer().getOfflinePlayer(playerName);
-		long fp = offlinePlayer.getFirstPlayed();
-		long lp = offlinePlayer.getLastPlayed();
-		File waffleInfo = new File("players.yml");
-		try {
-			waffleInfo.createNewFile();
-			System.out.println("checkpoint a");
-		} catch (IOException err) {
-			err.printStackTrace();
-		}
-		if(waffleInfo.exists()){
-			System.out.println("checkpoint b");
-			try {
-				BufferedWriter writer = new BufferedWriter(new FileWriter(waffleInfo));
-				writer.write("Username: " + playerName + ", " + "IP: " + iP);
-				writer.newLine();
-				writer.write("First Login: " + fp + ", " + "Last Login: " + lp);
-				writer.newLine();
-				System.out.println("checkpoint c");
-				writer.flush();
-				writer.close();
-				System.out.println("checkpoint d");
-			} catch (FileNotFoundException err) {
-				System.out.println("File Not Found. Good Luck!");
-			} catch (IOException err) {
-				err.printStackTrace();
-			}
-		}
 	}
 
 	@Override
---
+++ WaffleMarm/src/com/marmalade/wafflemarm/WaffleSpyEvent.java
@@ -0,0 +1,59 @@
+package com.marmalade.wafflemarm;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.InetAddress;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+
+public class WaffleSpyEvent implements Listener{
+	
+	WaffleMarm plugin;
+
+	WaffleSpyEvent(WaffleMarm instance) {
+		plugin = instance;
+	}
+	static File df = WaffleMarm.plugin.getDataFolder();
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void AsyncPlayerPreLoginEvent(AsyncPlayerPreLoginEvent e){
+		InetAddress ip = e.getAddress();
+		String iP = ip.getHostName();
+		String playerName = e.getName();
+		OfflinePlayer offlinePlayer = plugin.getServer().getOfflinePlayer(playerName);
+		long fp = offlinePlayer.getFirstPlayed();
+		long lp = offlinePlayer.getLastPlayed();
+		File waffleInfo = new File("players.yml");
+		try {
+			waffleInfo.createNewFile();
+			System.out.println("checkpoint a");
+		} catch (IOException err) {
+			err.printStackTrace();
+		}
+		if(waffleInfo.exists()){
+			System.out.println("checkpoint b");
+			try {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(waffleInfo));
+				writer.write("Username: " + playerName + ", " + "IP: " + iP);
+				writer.newLine();
+				writer.write("First Login: " + fp + ", " + "Last Login: " + lp);
+				writer.newLine();
+				System.out.println("checkpoint c");
+				writer.flush();
+				writer.close();
+				System.out.println("checkpoint d");
+			} catch (FileNotFoundException err) {
+				System.out.println("File Not Found. Good Luck!");
+			} catch (IOException err) {
+				err.printStackTrace();
+			}
+		}
+	}
+}
