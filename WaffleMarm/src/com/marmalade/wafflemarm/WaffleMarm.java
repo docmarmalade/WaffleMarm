a45ce1453ffb60d114e425f608b3529f71868151
docmarmalade <mark.meyer1014@gmail.com>
2014-05-05 13:08:46 -0400

    updated

--- WaffleMarm/src/com/marmalade/wafflemarm/WaffleMarm.java
+++ WaffleMarm/src/com/marmalade/wafflemarm/WaffleMarm.java
@@ -1,39 +1,64 @@
 package com.marmalade.wafflemarm;
 
+import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Logger;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.marmalade.wafflemarm.WaffleSpyCommand;
+
 public class WaffleMarm extends JavaPlugin {
 
 	static WaffleMarm plugin;
 	public final Logger log = Logger.getLogger("Minecraft");
 	PlayerListener playerListener = new PlayerListener(this);  
 	BlockListener blockListener = new BlockListener(this);
-    WaffleSpyEvent waffleSpyEvent = new WaffleSpyEvent(this);
+    WaffleSpyCommand waffleSpyCommand = new WaffleSpyCommand(this);
 
 	public WaffleMarm() {
 		plugin = this;
 	}
- 
+	
+	File waffleInfo;
+	FileConfiguration fileConfig = new YamlConfiguration();
+
+	 public void loadYamls() {
+	        try {
+	            fileConfig.load(waffleInfo); 
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	 
+	 public void saveYamls() {
+	        try {
+	            fileConfig.save(waffleInfo); //saves the FileConfiguration to its File
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+
 	@Override
 	public void onEnable(){
 		log.info("It's Alive!!");
 		PluginManager pm = getServer().getPluginManager();
-
+		
 		if (Config.loadSettings()) {
 			getCommand("hi").setExecutor(new Commands(this));    //registers command executors
 			getCommand("jump").setExecutor(new Commands(this));
-			getCommand("seen").setExecutor(new WaffleSpyCommand(this));
 		    System.out.print("WaffleMarm Plugin Enabled!");
 		} 
+		loadYamls();
+		saveYamls();
 		saveConfig();
 
 		pm.registerEvents(playerListener, this);       //registers eventlisteners
 		pm.registerEvents(blockListener, this);
-		pm.registerEvents(waffleSpyEvent,this);
+		pm.registerEvents(waffleSpyCommand,this);
 	}
 
 	public static Set<String> oldChunks = new HashSet<String>();   //2 hashsets for storing coord sets of chunks
--- WaffleMarm/src/com/marmalade/wafflemarm/WaffleSpyCommand.java
+++ WaffleMarm/src/com/marmalade/wafflemarm/WaffleSpyCommand.java
@@ -4,19 +4,75 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 
-public class WaffleSpyCommand implements CommandExecutor {
+
+
+public class WaffleSpyCommand implements CommandExecutor, Listener {
 
 	WaffleMarm plugin;
 
 	WaffleSpyCommand(WaffleMarm instance) {
 		plugin = instance;
+	}
+
+	public WaffleSpyCommand() {
+		// TODO Auto-generated constructor stub
+	}
+
+	WaffleSpyCommand SpyLogin = new WaffleSpyCommand();
+	
+	@EventHandler(priority = EventPriority.HIGHEST)             
+	public void onPlayerLogin(PlayerLoginEvent event){
+		String name = event.getPlayer().getName(); 
+		File waffleInfo = new File ("plugins"+File.separator+"WaffleMarm"+File.separator+"players"+File.separator+name);
+		FileConfiguration fileConfig = new YamlConfiguration();
+		String ip = event.getAddress().toString();
+		OfflinePlayer offlinePlayer = event.getPlayer();
+		String UUID = ((Player) offlinePlayer).getUniqueId().toString();;
+		SimpleDateFormat sdf = new SimpleDateFormat("MMMM d, yyyy 'at' h:mm a");
+		String firstDate = sdf.format(offlinePlayer.getFirstPlayed());
+		String lastDate = null;
+		if(offlinePlayer.hasPlayedBefore()){
+			lastDate = sdf.format(offlinePlayer.getLastPlayed());
+		}
+
+		if(!waffleInfo.exists()){
+			try {
+				waffleInfo.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				fileConfig = YamlConfiguration.loadConfiguration(waffleInfo);
+				fileConfig.set("Username:", name);
+				fileConfig.set("IP:", ip);
+				fileConfig.set("UUID:", UUID);
+				fileConfig.set("FirstLogin:", firstDate);
+				fileConfig.set("LastLogin:", lastDate);
+				fileConfig.save(waffleInfo);
+				Bukkit.getLogger().info(waffleInfo.getPath());
+			} catch (FileNotFoundException err) {
+				System.out.println("File Not Found. Good Luck!");
+			} catch (IOException err) {
+				err.printStackTrace();
+			}
+		}
+		Bukkit.getLogger().info("Created a new instance of players.yml: " + name);	
 	}
 
 	@Override
@@ -28,7 +84,7 @@ public class WaffleSpyCommand implements CommandExecutor {
 			if(player.isOp()){
 				if (trimmedArgs.length == 1) {
 					sender.sendMessage(ChatColor.GOLD + "Player information for " + ChatColor.GREEN + trimmedArgs[0]);
-					File waffleInfo = new File("players.yml");
+					File waffleInfo = new File ("plugins"+File.separator+"WaffleMarm"+File.separator+"players"+File.separator+trimmedArgs[0]);
 					OfflinePlayer offlinePlayer = plugin.getServer().getOfflinePlayer(trimmedArgs[0]);
 					long fp = offlinePlayer.getFirstPlayed();
 					try {
@@ -51,8 +107,16 @@ public class WaffleSpyCommand implements CommandExecutor {
 					}
 				}
 			}
+			else{
+				player.sendMessage("You do not have permission to do this.");
+			}
 			return true;
 		}
 		return false;
-	}
+	}		
 }
+
+
+
+
+
